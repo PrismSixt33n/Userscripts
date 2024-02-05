e8ff74e334e1a4023a69a5c887b902ef32363800
PrismSixt33n <157962677+PrismSixt33n@users.noreply.github.com>
2024-02-05 22:55:56 +0000

    Update PTPTMDBHelper.user.js

--- PTPTMDBHelper.user.js
+++ PTPTMDBHelper.user.js
@@ -12,7 +12,7 @@
 
 
 /// API KEY GOES BELOW 'INBETWEEN_THESE'
-let apiKey = '75c8f6d3dd058fe33f10db544d0cbb6b';
+let apiKey = '#########';
 /// API KEY GOES ABOVE 'INBETWEEN_THESE'
 
 if (!apiKey || apiKey.trim() === '') {
