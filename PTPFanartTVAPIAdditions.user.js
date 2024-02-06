539585cbbbec713ef9c4e6d28065ce2d4bbe9c5c
PrismSixt33n <157962677+PrismSixt33n@users.noreply.github.com>
2024-02-06 05:01:02 +0000

    Update PTPFanartTVAPIAdditions.user.js
    v1.1

--- PTPFanartTVAPIAdditions.user.js
+++ PTPFanartTVAPIAdditions.user.js
@@ -1,16 +1,18 @@
 // ==UserScript==
-// @name         Fanart.tv API Additions
-// @version      1
+// @name         PTP Fanart.tv API
+// @version      1.1
 // @grant        GM_xmlhttpRequest
 // @grant        GM.notification
 // @match        https://passthepopcorn.me/torrents.php*
-// @description  Adds additional artwork to PTP from the Fanart.tv API
+// @description  Fanart.tv API
 // @icon        https://passthepopcorn.me/favicon.ico
 // @author       Prism16
 // ==/UserScript==
 
-let apiKey = '';
-var fanartapiKey = "";
+let apiKey = '###############';
+var fanartapiKey = "###################";
+var isPanelVisible = false;
+
 (function() {
     'use strict';
 window.addEventListener('load', function() {
@@ -27,41 +29,59 @@ xhr.onreadystatechange = function () {
         if (response.results && response.results.length > 0) {
             var movieId = response.results[0].id;
             var url = "https://webservice.fanart.tv/v3/movies/" + movieId + "?api_key=" + fanartapiKey;
-            fetch(url)
-                .then(response => response.json())
-                .then(data => {
-                    var englishArtwork = data.moviebanner.filter(artwork => artwork.lang === "en");
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        var englishArtwork = data.moviebanner.filter(artwork => artwork.lang === "en");
 
-                    if (englishArtwork && englishArtwork.length > 0) {
-                        var bannerUrl = englishArtwork[0].url;
-                        var newPanel2 = document.createElement('div');
-                        newPanel2.className = 'panel';
-                        newPanel2.id = 'banner_panel';
-                        var panelHeading2 = document.createElement('div');
-                        panelHeading2.className = 'panel__heading';
-                        var title2 = document.createElement('span');
-                        title2.className = 'panel__heading__title';
-                        var fanarttv2 = document.createElement('span');
-                        fanarttv2.style.color = '#50C2BF';
-                        fanarttv2.textContent = 'Fanart.tv';
-                        title2.appendChild(fanarttv2);
-                        title2.appendChild(document.createTextNode(' Banner'));
-                        panelHeading2.appendChild(title2);
-                        newPanel2.appendChild(panelHeading2);
-                        var panelBody2 = document.createElement('div');
-                        panelBody2.className = 'panel__body';
-                        panelBody2.style.position = 'relative';
-                        var bannerImg = document.createElement('img');
-                        bannerImg.src = bannerUrl;
-                        bannerImg.style.height = 'auto';
-                        bannerImg.style.width = '100%';
-                        panelBody2.appendChild(bannerImg);
-                        newPanel2.appendChild(panelBody2);
-                        var sidebar2 = document.querySelector('div.sidebar');
-                        sidebar2.insertBefore(newPanel2, sidebar2.childNodes[4]);
-                    } else {
-                        console.log("No English language movie banner found for this movie.");
-                    }
+        if (englishArtwork && englishArtwork.length > 0) {
+            var bannerUrl = englishArtwork[0].url;
+            var newPanel2 = document.createElement('div');
+            newPanel2.className = 'panel';
+            newPanel2.id = 'banner_panel';
+            var panelHeading2 = document.createElement('div');
+            panelHeading2.className = 'panel__heading';
+            var title2 = document.createElement('span');
+            title2.className = 'panel__heading__title';
+            var fanarttv2 = document.createElement('span');
+            fanarttv2.style.color = '#50C2BF';
+            fanarttv2.textContent = 'Fanart.tv';
+            title2.appendChild(fanarttv2);
+            title2.appendChild(document.createTextNode(' Banner'));
+
+            // Create the toggle element
+            var toggle2 = document.createElement('a');
+            toggle2.className = 'panel__heading__toggler';
+            toggle2.title = 'Toggle';
+            toggle2.href = '#';
+            toggle2.textContent = 'Toggle';
+
+
+
+            toggle2.onclick = function() {
+                var panelBody = document.querySelector('#banner_panel .panel__body');
+                panelBody.style.display = (panelBody.style.display === 'none') ? 'block' : 'none';
+                return false;
+            };
+
+            panelHeading2.appendChild(title2);
+            panelHeading2.appendChild(toggle2);
+            newPanel2.appendChild(panelHeading2);
+            var panelBody2 = document.createElement('div');
+            panelBody2.className = 'panel__body';
+            panelBody2.style.position = 'relative';
+            panelBody2.style.display = isPanelVisible ? 'block' : 'none';
+            var bannerImg = document.createElement('img');
+            bannerImg.src = bannerUrl;
+            bannerImg.style.height = 'auto';
+            bannerImg.style.width = '100%';
+            panelBody2.appendChild(bannerImg);
+            newPanel2.appendChild(panelBody2);
+            var sidebar2 = document.querySelector('div.sidebar');
+            sidebar2.insertBefore(newPanel2, sidebar2.childNodes[4]);
+        } else {
+            console.log("No English language movie banner found for this movie.");
+        }
 if (data.hdmovielogo && data.hdmovielogo.length > 0) {
     var englishArtwork2 = data.hdmovielogo.filter(artwork => artwork.lang === "en");
 
@@ -69,7 +89,7 @@ if (data.hdmovielogo && data.hdmovielogo.length > 0) {
         var logoUrl = englishArtwork2[0].url;
         var newPanel = document.createElement('div');
         newPanel.className = 'panel';
-        newPanel.id = 'parents_guide';
+        newPanel.id = 'fanartpanel';
         var panelHeading = document.createElement('div');
         panelHeading.className = 'panel__heading';
         var title = document.createElement('span');
@@ -79,11 +99,26 @@ if (data.hdmovielogo && data.hdmovielogo.length > 0) {
         fanarttv.textContent = 'Fanart.tv';
         title.appendChild(fanarttv);
         title.appendChild(document.createTextNode(' Logo'));
+
+        var toggle = document.createElement('a');
+        toggle.className = 'panel__heading__toggler';
+        toggle.title = 'Toggle';
+        toggle.href = '#';
+        toggle.textContent = 'Toggle';
+
+        toggle.onclick = function() {
+            var panelBody = document.querySelector('#fanartpanel .panel__body');
+            panelBody.style.display = (panelBody.style.display === 'none') ? 'block' : 'none';
+            return false;
+        };
+
         panelHeading.appendChild(title);
+        panelHeading.appendChild(toggle);
         newPanel.appendChild(panelHeading);
         var panelBody = document.createElement('div');
         panelBody.className = 'panel__body';
         panelBody.style.position = 'relative';
+        panelBody.style.display = isPanelVisible ? 'block' : 'none';
         var logoImg = document.createElement('img');
         logoImg.src = logoUrl;
         logoImg.style.width = '100%';
